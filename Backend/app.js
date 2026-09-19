18210dda5fa5698468be976577fb3a4ee959e9e8
Ishtiaque Ali <166026053+IshtiaqueDev@users.noreply.github.com>
2026-09-19 21:58:18 +0500

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -21,7 +21,7 @@ app.use(session({
     resave:false,
     saveUninitialized:true,
     cookie:{
-        maxAge:7 * 24 * 60 * 60 * 1000,
+        maxAge:7 * 24 * 600 * 60 * 1000,
         httpOnly:true,
         secure:true,
         sameSite:"none"
